34d9f9c354853704fd1dbd9f263fa5a62a14deab
wbin <mrlwbin@163.com>
2018-10-30 21:34:51 +0800

    测试

--- src/webserver/main.go
+++ src/webserver/main.go
@@ -8,7 +8,7 @@ import (
 func firstPage(w http.ResponseWriter,r *http.Request){
 	io.WriteString(w,"<h1>hello,this is my first page</h1>" +
 		"<h2>we changed a little to test deployserver</h2>" +
-		"sh")
+		"sh,is deploy server work？")
 }
 
 func main(){
