f11f1b90d769703a4d2c9f01e6d959c8b694d2b8
wbin <mrlwbin@163.com>
2018-10-30 21:40:44 +0800

    add

--- src/webserver/main.go
+++ src/webserver/main.go
@@ -8,7 +8,8 @@ import (
 func firstPage(w http.ResponseWriter,r *http.Request){
 	io.WriteString(w,"<h1>hello,this is my first page</h1>" +
 		"<h2>we changed a little to test deployserver</h2>" +
-		"sh,is deploy server work？")
+		"sh,is deploy server work？" +
+		"<div>God,is this work?</div>")
 }
 
 func main(){
